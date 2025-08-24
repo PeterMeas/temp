1f682773264b95a57ab22a6196c07db775612e43
Peter <test@MB-AIR.local>
2025-08-23 21:37:40 -0400

    RPS complete

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 
 
+
 function getComputerChoice() {
     let result;
     let rock = "rock"; // 0
@@ -18,56 +19,71 @@ function getHumanChoice() {
     return humanChoice;
 }
 
-function playGame() {
+let humanScore = 0, computerScore = 0;
+let roundCount = 0;
 
-    let humanScore = 0, computerScore = 0;
+    let rock = document.querySelector('#rock');
+    let paper = document.querySelector('#paper');
+    let scissors = document.querySelector('#scissors');
 
+    rock.addEventListener('click', () => {
+        playRound('rock', getComputerChoice());
+    })
+    paper.addEventListener('click', () => {
+        playRound('paper', getComputerChoice());
+    })
+    scissors.addEventListener('click', () => {
+        playRound('scissors', getComputerChoice());
+    })
 
+    
+    
+    const results = document.querySelector('#result');
+    const score = document.querySelector('#score');
+
+    function updateScore() {
+        score.textContent = `Computer: ${computerScore} | You: ${humanScore}`;
+    }
     function playRound(humanChoice, computerChoice) {
         
-        
-        switch (humanChoice) {
-            case "rock":
-                if (computerChoice === "rock") {
-                    console.log("Tie");
-                } else if (computerChoice === "paper") {
-                    console.log("Paper beats rock, you lose!");
-                    computerScore += 1;
-                } else {
-                    console.log("You win! rock beats scissors");
-                    humanScore += 1;
-                }
-                break;
-            case "paper":
-                if (computerChoice === "rock") {
-                    console.log("You win! paper beats rock");
-                    humanScore += 1;
-                } else if (computerChoice === "paper") {
-                    console.log("Tie");
-                } else {
-                    console.log("You lose!, scissors beat paper!");
-                    computerScore += 1;
-                }
-                break;
-            case "scissors":
-                if (computerChoice === "rock") {
-                    console.log("You lose, rock beats scissors");
-                    computerScore +=1;
-                } else if (computerChoice === "paper") {
-                    console.log("You win! scissors beat paper");
-                    humanScore +=1;
-                } else {
-                    console.log("Tie!");
-                }
-                break;
+        let message = `You: ${humanChoice} | Computer: ${computerChoice} --- `;        
+        if(roundCount <= 5) {
+        if (humanChoice === computerChoice) {
+            message += "It's a tie";
+    
+        } else if (
+            (humanChoice === "rock" && computerChoice === "scissors") ||
+            (humanChoice === "paper" && computerChoice === "rock") ||
+            (humanChoice === "scissors" && computerChoice === "paper")) {
+                    message += `You win! ${humanChoice} beats ${computerChoice}`;
+                humanScore += 1;
+    
+        } else {
+            message += `You lose! ${computerChoice} beats ${humanChoice}`;
+            computerScore += 1;
+    
             }
-    }
+        } else {
+            result.textContent = "Game Over! Refresh to play again.";
+            return;
+        }
+            roundCount += 1;
+            results.textContent = message;
+            updateScore();    
+        }
+        
+updateScore();
 
-    for (let i = 0; i < 5; i++) {
+
+function playGame() {
+
+
+    
+    //for (let i = 0; i < 5; i++) {
         let computerChoice = getComputerChoice();
         let humanChoice = getHumanChoice().toLowerCase();
         playRound(humanChoice,computerChoice);
-    }
+    //}
     if (computerScore > humanScore) {
         console.log(`LOSE!! Computer: ${computerScore} | You: ${humanScore}`)
     } else if (humanScore > computerScore){
@@ -77,3 +93,4 @@ function playGame() {
     }
 
 }
+
